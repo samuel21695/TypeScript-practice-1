85d5f390306cc9b523c945585d0d770f37211704
samuel21695 <samuel21695@gmail.com>
2023-12-03 23:41:57 +0900

    tuple type for array

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 "use strict";
-// 함수에 타입 지정
-// 파라미터로 무조건 number 리턴 값도 number
-function 함수(x) {
-    return x * 2;
-}
+var john = [123, true];
--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-// 함수에 타입 지정
-// 파라미터로 무조건 number 리턴 값도 number
-function 함수(x :number) :number{
-  return x * 2
-}
+// array에 쓰는 tuple type
+type Member = [number, boolean];
+let john:Member = [123, true]
