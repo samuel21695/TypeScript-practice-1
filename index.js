044b32f4d21b4143e2274a7a69a84e48e90abe45
samuel21695 <samuel21695@gmail.com>
2023-12-03 23:34:15 +0900

    array type

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 "use strict";
-var 이름 = 'kim';
+// array 타입 지정
+// string이라는 타입만 가능
+var 이름 = ['kim', 'park'];
 이름 = 123;
--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
-let 이름 :string = 'kim'
+// array 타입 지정
+// string이라는 타입만 가능
+let 이름 :string[]= ['kim', 'park'];
 이름 = 123;
